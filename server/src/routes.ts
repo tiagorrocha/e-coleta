9b37f243ada7ad2b94b17b5109c0e841f063d914
Tiago Rocha <tiagorocha.tiago@gmail.com>
2020-06-02 22:00:16 -0300

    [feat] Implement server

---
+++ server/src/routes.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import PointsController from './controller/PointsController';
+import ItemsController from './controller/ItemsController';
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items',itemsController.index);
+routes.post('/points',pointsController.create);
+routes.get('/points',pointsController.index);
+routes.get('/points/:id',pointsController.show);
+
+export default routes;
